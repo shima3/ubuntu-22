7410c2a732cb9f357c254476290fcbe93cefa370
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-04-17 16:54:32 +0900

    edulinux/bin/backup.sh: export -> cp

--- edulinux/bin/backup.sh
+++ edulinux/bin/backup.sh
@@ -5,4 +5,7 @@ cd "$bin/.."
 base="$(basename $PWD)"
 date="$(date +%Y%m%d)"
 
-time docker export "$base" --output "$base-$date.tar"
+# docker exportではマウントしたボリュームが保存されない。
+# docker export "$base" --output "$base-$date.tar"
+
+time docker cp --archive "$base:/" - > "$base-$date.tar"
