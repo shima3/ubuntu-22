aca52cb5d038d3c20f8ebb51dc1dedad6905a4ea
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-04-19 13:53:07 +0900

    add gimp

--- pkg2/gimp/bin/test.sh
+++ pkg2/gimp/bin/test.sh
@@ -3,7 +3,10 @@ bin="${0%/*}"
 cd "$bin/.."
 base="${PWD##*/}"
 
+<<<<<<< Updated upstream
 if ! docker build --tag "test:$base" trash; then exit 1; fi
 
+=======
+>>>>>>> Stashed changes
 # docker run -it --rm -p 3333:3389 "test:$base" bash -c 'useradd -m -s /bin/bash student; echo student:1883shima | chpasswd; /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf'
 docker run -it --rm -p 3333:3389 "test:$base"
