152bb62f2f2735add6b4e9ba5b8a52aff03c1aea
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-04-20 21:48:49 +0900

    remove trash

--- trash/git/bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bin="${0%/*}"
-cd "$bin/.."
-base="${PWD##*/}"
-osver="$(basename ${PWD%/*/*})"
-
-# docker run -it --rm -v "$PWD:/$PWD" -w "$PWD" "test:$base" bash
-docker run -it --rm "test:$base" bash
