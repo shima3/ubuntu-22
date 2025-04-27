9e4a7e01a5e5c9c767796817e3c8b14e63ee08bb
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-04-27 15:14:24 +0900

    modify: edulinux/bin/passwd_shima.sh

--- edulinux/bin/passwd_shima.sh
+++ edulinux/bin/passwd_shima.sh
@@ -3,4 +3,5 @@ script="$(readlink -f $0)"
 bin="${script%/*}"
 cd "$bin/.."
 bin/exec.sh useradd -m -s /bin/bash shima
-bin/exec.sh usermod --password '$y$j9T$ejpZCTUF/.oOCoWFC0Zpm0$cMqkCL6K7JiiF9BnQQwk8FP3bFkKsRtX//GidKXxuiB' shima
+bin/exec.sh usermod --password '$y$j9T$whNZTgxbGdeIJuWEUSkJA0$0fr5b1BLfAbV4qd8GMzM8JOg5vle2spWcuUI3xW9jCD' shima
+
