566c790461aaa7874cd171208179c1dcf3a119b0
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-05-02 07:01:38 +0900

    modified:   bin/build.sh

--- edulinux/bin/build.sh
+++ edulinux/bin/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+list=`docker ps -q -f status=exited; docker ps -q -f status=created`
+if [ "$list" != "" ]; then docker rm $list; fi
+list="$(docker images -f dangling=true -q)"
+if [ "$list" != "" ]; then docker rmi $list; fi
+
 script="$(readlink -f $0)"
 bin="${script%/*}"
 # cd "${0%/*}/.."
@@ -13,16 +18,13 @@ osver="${root##*/}"
 # docker tag "$img" ubuntu
 
 $root/base/bin/pull.sh
-for dir in $root/pkg/*; do $dir/bin/pull.sh; done
+# for dir in $root/pkg/*; do $dir/bin/pull.sh; done
+for pkg in $(awk '/^COPY --from=pkg_/{ print substr($2, 12); }' Dockerfile); d
+o $pkg/bin/pull.sh; done
 
 list="$(docker ps --filter ancestor=$base -q)"
 if [ "$list" != "" ]; then docker stop $list; fi
 
 docker build --tag "$base" --label "OS-Ver=$osver" --file Dockerfile context
-
-list=`docker ps -q -f status=exited; docker ps -q -f status=created`
-if [ "$list" != "" ]; then docker rm $list; fi
-list="$(docker images -f dangling=true -q)"
-if [ "$list" != "" ]; then docker rmi $list; fi
 
 echo -e "\a\a\a"
