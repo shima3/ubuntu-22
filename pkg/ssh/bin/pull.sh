c0dad858bdd72d0b5c219da1a5ec5a981dd4dda4
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-05-01 22:26:23 +0900

    new file:   20-chromium/bin/pull.sh 	new file:   20-emacs/bin/pull.sh 	new file:   20-gimp/bin/pull.sh 	new file:   20-mate/bin/pull.sh 	new file:   20-pdf/bin/pull.sh 	new file:   20-vscode/bin/pull.sh 	new file:   30-japanese/bin/pull.sh 	new file:   gcc/bin/pull.sh 	new file:   jdk/bin/pull.sh 	new file:   latex/bin/pull.sh 	new file:   python/bin/pull.sh 	new file:   ssh/bin/pull.sh 	new file:   utils/bin/pull.sh

---
+++ pkg/ssh/bin/pull.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+reg="kshima"
+script="$(readlink -f $0)"
+bin="${script%/*}"
+cd "$bin/.."
+# base="${PWD##*/[0-9]?-}"
+base="${PWD##*/}"
+osver="$(basename ${PWD%/*/*})"
+local_img="pkg:$base"
+
+id=($(docker images "${osver/-/:}.04" --format '{{.ID}}'))
+arch="$(docker inspect $id --format '{{.Architecture}}')"
+# echo "$arch"
+remote_img="$reg/$osver-${local_img}_$arch"
+# echo "$img"
+
+if ! remote_created="$(skopeo inspect docker://$remote_img 2> /dev/null | jq --raw-output .Created)"; then
+    echo "$remote_img is not found"
+    exit 1
+fi
+echo "$remote_created: $remote_img"
+
+if local_created="$(docker image inspect $local_img --format '{{.Created}}' 2> /dev/null)"; then
+    echo "$local_created: $local_img"
+    if ! [[ "$local_created" < "$remote_created" ]]; then
+        echo "$local_img is newer than or equal to $remote_img"
+        exit 0
+    fi
+fi
+
+docker pull "$remote_img"
+docker tag "$remote_img" "$local_img"
