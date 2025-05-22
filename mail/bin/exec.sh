ba709da2c4a0d83808e7c62aa8c0dae9349fa475
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-05-22 16:16:21 +0900

    modified:   bin/exec.sh

--- mail/bin/exec.sh
+++ mail/bin/exec.sh
@@ -4,8 +4,13 @@ script="$(readlink -f $0)"
 bin="${script%/*}"
 cd "$bin/.."
 base=$(basename $PWD)
+
+# 前回起動したコンテナがあれば削除する。
+list="$(docker ps --all --filter ancestor=$base --quiet)"
+
 if [[ "$*" == "" ]]
 then cmd="bash"
 else cmd="$*"
 fi
-docker exec -it $base $cmd
+
+docker exec -it $list $cmd
