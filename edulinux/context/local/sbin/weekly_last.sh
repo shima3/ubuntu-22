4975faf28aa980a9333a109b80de0195865f0f1f
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-06-29 20:55:13 +0900

    modified: /usr/local/sbin/weekly_last.sh

--- edulinux/context/local/sbin/weekly_last.sh
+++ edulinux/context/local/sbin/weekly_last.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 cd /home/shima
 dayofweek="$1"
 touch "$dayofweek.last"
-xrdp_last.sh | while read -r user display ip login minus logout; do if [[ "$user" =~ [hj]20[0-9]?? && "$(LC_ALL=C date -d $login +%a)" == "$dayofweek" ]]; then echo -e "$user\t$ip\t$login\t$logout"; fi; done | sort "$dayofweek.last" - | uniq > "/tmp/$dayofweek.last"
+xrdp_last.sh | while read -r user display ip login minus logout; do if [[ "$user" =~ [hj]20[0-9]?? && "$(LC_ALL=C date -d $login +%a)" == "$dayofweek" ]]; then echo -e "$user\t$login\t$ip"; fi; done | sort "$dayofweek.last" - | uniq > "/tmp/$dayofweek.last"
 mv "/tmp/$dayofweek.last" .
-chown shima "$dayofweek.last"
+chown shima.sudo "$dayofweek.last"
