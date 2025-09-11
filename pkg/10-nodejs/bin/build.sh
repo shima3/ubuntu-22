6f671a193d8a9fe21d98bd5e1b2e3e37a7b14a5c
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-09-11 13:00:14 +0900

    new file:   pkg/10-nodejs

---
+++ pkg/10-nodejs/bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# PACKAGE="apt-transport-https ca-certificates curl gnupg nodejs"
+PACKAGE="gnupg nodejs"
+
+script="$(readlink -f $0)"
+bin="${script%/*}"
+# cd "${0%/*}/.."
+cd "$bin/.."
+# base="${PWD##*/}"
+base="${PWD##*/[0-9]?-}"
+root="${PWD%/*/*}"
+osver="${root##*/}"
+
+$root/base/bin/pull.sh
+
+if ! docker build --tag "test:$base" --target test-stage --build-arg "PACKAGE=$PACKAGE" --file Dockerfile context; then exit 1; fi
+
+if ! docker build --tag "pkg:$base" --build-arg "PACKAGE=$PACKAGE" --target package-stage --label "OS-Ver=$osver" --file Dockerfile context; then exit 1; fi
