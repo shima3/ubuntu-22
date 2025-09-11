fde9f2e1fd326a2ad175200d9a6f67c5ce81068b
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-09-11 20:01:33 +0900

    modified:   edulinux

--- edulinux/bin/test.sh
+++ edulinux/bin/test.sh
@@ -20,20 +20,20 @@ bin/_run.sh \
 # docker exec "$base" reset-password.sh guest guest
 # echo 'Guest' | docker exec --interactive --user guest "$base" config-git.sh guest@e.hiroshima-cu.ac.jp
 
-bin/exec.sh useradd -g sudo -m --key HOME_MODE=0755 -s /bin/bash shima
+bin/exec.sh useradd -g sudo -m --key HOME_MODE=0751 -s /bin/bash shima
 bin/exec.sh usermod --password '$y$j9T$whNZTgxbGdeIJuWEUSkJA0$0fr5b1BLfAbV4qd8GMzM8JOg5vle2spWcuUI3xW9jCD' shima
 echo 'Kazuyuki Shima' | docker exec --interactive --user shima "$base" config-git.sh shima@hiroshima-cu.ac.jp
 
 # user=a20999
 user=j90001
 # bin/exec.sh useradd -g users -m -s /bin/bash "$user"
-bin/exec.sh useradd -g users -m --key HOME_MODE=0755 -s /bin/bash "$user"
+bin/exec.sh useradd -g users -m --key HOME_MODE=0751 -s /bin/bash "$user"
 bin/exec.sh usermod --password '$y$j9T$4nNEnBJJRMYu9XGqCCTl/0$a9hGDNeBuh4OL4wB5AaHv2.AN2peG8A5uYoK7nwP0U7' "$user"
 echo 'Dummy User a' | docker exec --interactive --user "$user" "$base" config-git.sh "$user@e.hiroshima-cu.ac.jp"
 
 # user=b20999
 user=j90002
-bin/exec.sh useradd -g users -m --key HOME_MODE=0755 -s /bin/bash "$user"
+bin/exec.sh useradd -g users -m --key HOME_MODE=0751 -s /bin/bash "$user"
 bin/exec.sh usermod --password '$y$j9T$4nNEnBJJRMYu9XGqCCTl/0$a9hGDNeBuh4OL4wB5AaHv2.AN2peG8A5uYoK7nwP0U7' "$user"
 echo 'Dummy User b' | docker exec --interactive --user "$user" "$base" config-git.sh "$user@e.hiroshima-cu.ac.jp"
 
--- edulinux/context/local/sbin/reset-password.sh
+++ edulinux/context/local/sbin/reset-password.sh
@@ -6,7 +6,7 @@ then
     echo "Usage: ${0##*/} LOGIN [PASSWORD]"
     exit 0
 fi
-useradd -g users -m --key HOME_MODE=0755 -s /bin/bash $user
+useradd -g users -m --key HOME_MODE=0751 -s /bin/bash $user
 if [ "$pw" == "" ]
 then pw=$(pwgen -Bs1)
 fi
