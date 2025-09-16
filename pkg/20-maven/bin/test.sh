19b298987b75f750f8a1960fc398c3eac299b77c
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-09-16 16:14:39 +0900

    renamed:    10-jdk/ -> 10-java/ 	renamed:    90-python/ -> 10-python/ 	deleted:    20-maven/

--- pkg/20-maven/bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-script="$(readlink -f $0)"
-# bin="$(dirname $0)"
-bin="${script%/*}"
-cd "$bin/.."
-# base="$(basename $PWD)"
-base="${PWD##*/[0-9]?-}"
-
-docker run -it --rm -v "$PWD:/$PWD" -w "$PWD" "test:$base" bash
