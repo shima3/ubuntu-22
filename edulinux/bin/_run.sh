40c14386da0cae0777f12f60140cdd65ffd0c727
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-09-17 10:10:23 +0900

    modified:   edulinux/Dockerfile 	modified:   edulinux/bin/_run.sh 	modified:   edulinux/context/local/sbin/reset-password.sh

--- edulinux/bin/_run.sh
+++ edulinux/bin/_run.sh
@@ -11,6 +11,7 @@ xrdp_port=3333
 http_port=8888
 ssh_port=2222
 ttyd_port=7777
+nodejs_ports='3000-3009'
 
 # 前回起動したコンテナがあれば削除する。
 # list="$(docker ps --all --filter ancestor=$base --quiet)"
@@ -34,7 +35,7 @@ docker run \
        -p "0.0.0.0:$http_port:80" \
        -p "0.0.0.0:$ssh_port:22" \
        -p "0.0.0.0:$ttyd_port:7681" \
-       -p "0.0.0.0:3000-3099:3000-3099" \
+       -p "0.0.0.0:$nodejs_ports:$nodejs_ports" \
        --device /dev/fuse --cap-add SYS_ADMIN \
        --security-opt apparmor:unconfined \
        --shm-size="1gb" \
