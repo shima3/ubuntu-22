314ac0178f88259c4ceee42bc7fe9bbb8e3652cd
島和之 <shima@hiroshima-cu.ac.jp>
2025-09-24 14:10:20 +0900

    modified:   pkg/10-nodejs/bin/test.sh

--- pkg/10-nodejs/bin/test.sh
+++ pkg/10-nodejs/bin/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-port=3000
+# port=3000
 
 bin="$(dirname $0)"
 cd "$bin/.."
-# base=$(basename $PWD)
 base="${PWD##*/[0-9]?-}"
 
-list=$(docker ps --format json | jq --raw-output 'select(.Ports | test("^.*:'$port'->.*/tcp$")) | .ID')
-if [ "$list" != "" ]; then docker stop $list; fi
-docker-remove-containers-not-running
+# list=$(docker ps --format json | jq --raw-output 'select(.Ports | test("^.*:'$port'->.*/tcp$")) | .ID')
+# if [ "$list" != "" ]; then docker stop $list; fi
+# docker-remove-containers-not-running
 
-docker run --name "$base" --hostname "$base" -p "$port:3000" -e "LC_ALL=ja_JP.UTF-8" -it --rm "test:$base" bash
+# docker run --name "$base" --hostname "$base" -p "$port:3000" -e "LC_ALL=ja_JP.UTF-8" -it --rm "test:$base" bash
+docker run --name "$base" --hostname "$base" -e "LC_ALL=ja_JP.UTF-8" -it --rm "test:$base" bash
