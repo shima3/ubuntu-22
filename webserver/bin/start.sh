b0e97e992037efc06816d3ec6da15d1ef0de3ecd
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-09-30 20:22:31 +0900

    modified:   bin/start.sh

--- webserver/bin/start.sh
+++ webserver/bin/start.sh
@@ -22,8 +22,6 @@ $bin/_run.sh \
     --ulimit core=0 \
     --tmpfs /run --tmpfs /run/lock \
     --name "$base" \
-    -p "0.0.0.0:80:80" \
-    -p "0.0.0.0:22:22" \
     $base
 
 #    --mount "type=volume,src=$base-var-log,dst=/var/log" \
