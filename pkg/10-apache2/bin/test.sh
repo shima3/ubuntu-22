cae7700a247b5519d3d5cdb4f24e79cf37e768d2
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-10-01 22:10:42 +0900

    modified:   pkg/10-apache2/bin/test.sh

--- pkg/10-apache2/bin/test.sh
+++ pkg/10-apache2/bin/test.sh
@@ -4,7 +4,8 @@ port=8888
 
 bin="$(dirname $0)"
 cd "$bin/.."
-base=$(basename $PWD)
+# base=$(basename $PWD)
+base="${PWD##*/[0-9]?-}"
 
 list=$(docker ps --format json | jq --raw-output 'select(.Ports | test("^.*:'$port'->.*/tcp$")) | .ID')
 if [ "$list" != "" ]; then docker stop $list; fi
