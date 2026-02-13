dddcbff56827685f8ef37515c8d926f9028b6bfb
島和之 <shima@mac2025m4.local>
2026-02-13 16:51:52 +0900

    modified:   edulinux/Dockerfile 	modified:   edulinux/bin/start.sh

--- edulinux/bin/start.sh
+++ edulinux/bin/start.sh
@@ -81,4 +81,4 @@ docker exec -i "$base" bash -c "if [ -f $etctar ]; then echo restore: $etctar '-
 
 $bin/exec.sh useradd -g sudo -m --key HOME_MODE=0751 -s /bin/bash shima
 $bin/exec.sh usermod --password '$y$j9T$whNZTgxbGdeIJuWEUSkJA0$0fr5b1BLfAbV4qd8GMzM8JOg5vle2spWcuUI3xW9jCD' shima
-echo 'Kazuyuki Shima' | docker exec --interactive --user shima "$container" config-git.sh shima@hiroshima-cu.ac.jp
+echo 'Kazuyuki Shima' | docker exec --interactive --user shima "$base" config-git.sh shima@hiroshima-cu.ac.jp
