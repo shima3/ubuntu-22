44d60d47e8a700d3e6996cf42936aa65f9cc6f88
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2026-02-20 06:28:26 +0900

    modified:   bin/build.sh 	new file:   bin/make.sh 	modified:   context/apache2/conf.d/jupyter.conf

--- edulinux/bin/make.sh
+++ edulinux/bin/make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# if ! which skopeo > /dev/null; then
-#     echo 'skopeo: コマンドが見つかりません'
-#     exit 1
-# fi
+if ! which skopeo > /dev/null; then
+    echo 'skopeo: コマンドが見つかりません'
+    exit 1
+fi
 
 # 実行中ではないコンテナを削除する。
 list=`docker ps -q -f status=exited; docker ps -q -f status=created`
@@ -20,8 +20,14 @@ root="${PWD%/*}"
 # osver="$(basename ${PWD%/*})"
 osver="${root##*/}"
 
-# $root/base/bin/ensure.sh
-# for name in $(awk '/^COPY --from=pkg:/{ print substr($2, 12); }' Dockerfile); do $root/pkg/[0-9]?-$name/bin/ensure.sh; done
+# img="${osver/-/:}.04"
+# docker pull "$img"
+# docker tag "$img" ubuntu
+
+# $root/base/bin/pull.sh
+$root/base/bin/ensure.sh
+# for name in $(awk '/^COPY --from=pkg:/{ print substr($2, 12); }' Dockerfile); do $root/pkg/[0-9]?-$name/bin/pull.sh; done
+for name in $(awk '/^COPY --from=pkg:/{ print substr($2, 12); }' Dockerfile); do $root/pkg/[0-9]?-$name/bin/ensure.sh; done
 
 list="$(docker ps --filter ancestor=$base -q)"
 if [ "$list" != "" ]; then docker stop $list; fi
