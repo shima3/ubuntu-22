6bb40608fc0884d3ac77a614199cac1bbce2ef3e
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2026-07-13 09:09:07 +0900

    new file:   bin/start-ubuntu2026.sh

---
+++ edulinux/bin/start-ubuntu2026.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+reg="kshima"
+script="$(readlink -f $0)"
+# bin="${0%/*}"
+bin="${script%/*}"
+cd "$bin/.."
+base="${PWD##*/}"
+osver="$(basename ${PWD%/*})"
+ubuntu_img="${osver/-/:}.04"
+# tmpvol="$base-var-tmp"
+tmpvol="$base-home"
+# tmpdir="/var/tmp"
+tmpdir="/home"
+# etctar="$tmpdir/etc.tar"
+etctar="$tmpdir/.etc.tar"
+
+if ! which jq > /dev/null; then
+    echo 'jq: コマンドが見つかりません'
+    exit 1
+fi
+
+## Ubuntu のアーキテクチャを調べる。
+# docker pull "$ubuntu_img"
+# arch="$(docker inspect --format '{{.Architecture}}' --type=image $ubuntu_img)"
+# edulinux_img="$reg/$osver-$base:$arch"
+
+## /etc/{passwd,shadow,group,gshadow} を $etctar に保存する。
+if [[ "$(docker container inspect $base --format '{{json .Mounts}}' | jq --arg name $base-etc '.[] | select(.Name==$name)')" != "" ]]; then
+    echo backup: '/etc/{passwd,shadow,group,gshadow} ->' $etctar
+    if ! docker run --rm \
+    --mount "type=volume,src=$base-etc,dst=/mnt/etc" \
+    --mount "type=volume,src=$tmpvol,dst=$tmpdir" \
+    "$ubuntu_img" \
+    tar cf "$etctar" -C /mnt/etc passwd shadow group gshadow; then
+        exit 1
+    fi
+fi
+
+## 前回起動したコンテナを削除する。
+list="$(docker ps --all --filter name=$base --quiet)"
+if [[ "$list" != "" ]]; then
+    echo -n "remove $base container "
+    docker rm -f "$list"
+fi
+
+## ボリューム $base-etc を削除する。
+volume="$(docker volume inspect --format '{{.Name}}' $base-etc 2> /dev/null)"
+if [ "$volume" != "" ]; then
+    echo -n "remove volume: "
+    docker volume rm "$base-etc"
+fi
+
+student_ports='25001-25999'
+
+# docker pull "$edulinux_img"
+$bin/ensure.sh
+# docker cp --follow-link ldap:/container/service/slapd/assets/certs/ca.crt $HOME/ldap
+$bin/_run.sh \
+    -dit \
+    --restart unless-stopped \
+    --mount "type=volume,src=$base-var-log,dst=/var/log" \
+    --mount "type=volume,src=$base-var-tmp,dst=/var/tmp" \
+    --mount "type=volume,src=$base-backup,dst=/backup" \
+    --mount "type=volume,src=$base-etc,dst=/etc" \
+    --mount "type=volume,src=$base-home,dst=/home" \
+    --ulimit core=0 \
+    --tmpfs /run --tmpfs /run/lock \
+    --name "$base" \
+    --network mynet \
+    -v ldap_certs:/etc/sssd/certs \
+    --env-file $HOME/ldap/env \
+    -p "0.0.0.0:22:22" \
+    -p "0.0.0.0:$student_ports:$student_ports" \
+    $base
+
+#    -p "0.0.0.0:22:3389" \
+#    -v $HOME/ldap/ca.crt:/etc/sssd/ldap-ca.crt \
+#    -p "0.0.0.0:80:80" \
+#    "$edulinux_img"
+#    -p "0.0.0.0:443:3389"
+
+# hostname="$(hostname)"
+# docker exec -i "$base" config-apache2.sh "$hostname"
+docker exec -i "$base" config-apache2.sh
+# docker exec -i "$base" certbot-apache.sh shima@hiroshima-cu.ac.jp "$hostname"
+docker exec -i "$base" certbot-apache.sh shima@hiroshima-cu.ac.jp
+
+## $etctar から /etc/{passwd,shadow,group,gshadow} を復元する。
+docker exec -i "$base" bash -c "if [ -f $etctar ]; then echo restore: $etctar '-> /etc/{passwd,shadow,group,gshadow}'; tar xf $etctar -C /etc; fi"
+
+# docker cp $HOME/ldap/certs/ca.crt edulinux:/etc/sssd/ldap-ca.crt
+# docker exec "$base" chown root:root /etc/sssd/ldap-ca.crt
+# docker exec --detach "$base" bash -c "sssd > /var/log/sssd.out.log 2> /var/log/sssd.err.log"
