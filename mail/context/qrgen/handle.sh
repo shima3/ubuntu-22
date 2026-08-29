deeca61cc69531471118c5ac093376af8f8c6cf1
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2026-08-30 05:17:25 +0900

    modified:   mail/context/qrgen/handle.sh

--- mail/context/qrgen/handle.sh
+++ mail/context/qrgen/handle.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# printenv >> $HOME/handle.log
+LANG="ja_JP.UTF-8"
 IFS=$'\n' data=($(awk 'NR==1{print $2;}/^From: /{print substr($0, 7);}' | nkf))
 email="${data[0]}"
 name="${data[1]% <*}"
@@ -8,13 +10,14 @@ chmod g+r $HOME/handle.log
 if [[ "$domain" == "micron.com" || "$domain" =~ .hiroshima-cu.ac.jp || "$domain" == "hiroshima-cu.ac.jp" ]]
 then
     user="${email%@*}"
+    text="mailto:$name <$email>"
     qrcode="/tmp/mailto-$user.png"
-    java -cp "$HOME:$HOME/zxing/*" QRCodeGenerator "mailto:$name <$email>" "$qrcode"
-    mailx -s "QR code of your email address" -A "$qrcode" "$email" <<EOF
-メールアドレスを入力する手間を省くためのQRコードです。
+    java -cp "$HOME:$HOME/zxing/*" QRCodeGenerator "$text" "$qrcode"
+    mailx -s "QR code of $text" -A "$qrcode" -a "From: QR code generator <$RECIPIENT>" "$email" <<EOF
+あなたのメールアドレスを入力するためのQRコードを添付しました。
 本人や身分を証明するものではありません。
-定期試験や学割には使えません。
-ただし、公開しない方が無難です。
+例えば、定期試験や学割には使えません。
+しかし、他人には必要もなく見せない方が無難です。
 EOF
 fi
 exit 0
